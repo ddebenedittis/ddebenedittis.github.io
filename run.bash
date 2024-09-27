022280d733fdd77123ee558c4e58e9422fc4192b
Davide De Benedittis <davide.debenedittis@gmail.com>
2024-09-28 01:15:20 +0200

    First commit

---
+++ run.bash
@@ -0,0 +1,1 @@
+docker run -p 6969:80 astro_portfolio
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Davide De Benedittis | Personal Portfolio Website';
+export const SITE_DESCRIPTION = 'Personal portfolio webside.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
