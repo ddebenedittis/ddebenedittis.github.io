bf06e40688ec279a5f51a02c83cc27e164ef86b7
Davide De Benedittis <davide.debenedittis@gmail.com>
2024-10-01 19:41:14 +0200

    Change website image and description

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Davide De Benedittis | Personal Portfolio Website';
-export const SITE_DESCRIPTION = 'Personal portfolio webside.';
+export const SITE_DESCRIPTION = 'Personal portfolio webside, with my projects and published scientific papers.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
